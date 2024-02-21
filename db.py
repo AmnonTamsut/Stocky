7915fc4701052c59f47b05c75eb75eb4fc3971f9
AmnonTamsut <amnontamsut66@gmail.com>
2024-02-21 18:45:17 +0200

    Tinanainai

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from pymongo import MongoClient
 import bcrypt
 
@@ -7,14 +8,24 @@ db = client.users
 users = db.users
 
 
-def new_user(email, password):
+def new_user(user_email, password):
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    users.insert_one({"email": email, "password": hashed})
+    users.insert_one({"email": user_email, "password": hashed, "stocks": []})
+    a = 7
 
 
 def get_user(email):
     return users.find_one({"email": email})
+
+
+def get_user_id(unique_id):
+    return users.find_one({"_id": ObjectId(unique_id)})
+
+
+def add_stock(unique_id, symbol):
+    users.update_one({"_id": ObjectId(unique_id)}, {"$addToSet": {"stocks": symbol}})
+    print("success")
 
 
 def is_email_registered(email):
@@ -23,8 +34,8 @@ def is_email_registered(email):
     return False
 
 
-# new_user("a", "8543851")
-
-
+#add_stock("65d6143a8058aad7f3099819", "aapl")
+# new_user("a","1234")
 for p in users.find():
     print(p)
+    #users.delete_one(p)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
-from stock_price_retrieval import *
+from fastapi import FastAPI, HTTPException
 import db
+from stock_price_retrieval import *
 import auth
+from starlette.responses import RedirectResponse
+
 app = FastAPI()
 
 
@@ -12,18 +14,34 @@ async def root():
 
 @app.get('/login')
 async def login(email: str, password: str):
-    return auth.login(email,password)
+    if auth.login(email, password):
+        response = RedirectResponse(url="/dashboard")
+        return response
+    else:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
 
 
 @app.get('/dashboard')
 async def dashboard():
-    return {'dashboard': 'This is dashboard', 'data': 2}
+    return {'yochay': 'hachatich', 'matay?': "tamid"}
 
 
 @app.get('/api/stock')
 async def api(stock: str):
     return get_data(stock)
 
+
 @app.get('/api/stocks')
 async def api(stocks: str, start: str, end: str):
     return get_multiple_data(stocks, start, end)
+
+
+@app.put('/updatemystocks')
+async def update_stocks(unique_id: str, symbol: str):
+    try:
+        db.add_stock(unique_id, symbol)
+        return {"message": "Stock added successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
